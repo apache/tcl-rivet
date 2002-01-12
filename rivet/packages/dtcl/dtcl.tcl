14be3035df0f70ca29feb0a9b3a0d6405a2d8ac9
Damon Courtney <damonc@apache.org>
2002-01-12 01:35:19 +0000

    Adding a dtcl package to the packages directory.
    This package is a small compatibility layer between mod_dtcl and Rivet.
    
    A mod_dtcl user could conceivably convert to Rivet without changing any
    code by simply putting
    
    RivetServerConf ChildInitScript "package require Dtcl"
    
    In their configuration.  All of their mod_dtcl code should work without a
    change.

---
+++ rivet/packages/dtcl/dtcl.tcl
@@ -0,0 +1,38 @@
+###
+## This package is meant as a compatibility layer between Rivet and mod_dtcl.
+##
+## All of the mod_dtcl commands call their Rivet equivalents and return the
+## proper responses.
+###
+
+package provide Dtcl 1.0
+
+proc hgetvars {} {
+    uplevel {
+	load_env ENVS
+	load_cookies COOKIES
+    }
+}
+
+proc hputs {args} {
+    set nargs [llength $args]
+    if {$nargs < 1 || $nargs > 2} {
+	return -code error {wrong # args: should be "hputs ?-error? text"}
+    }
+
+    if {$nargs == 2} {
+	set string [lindex $args 1]
+    } else {
+	set string [lindex $args 0]
+    }
+
+    puts $string
+}
+
+proc hflush {} {
+    flush stdout
+}
+
+proc dtcl_info {} {
+    rivet_info
+}
---
+++ rivet/packages/dtcl/pkgIndex.tcl
@@ -0,0 +1,1 @@
+package ifneeded Dtcl 1.0 [list source [file join $dir dtcl.tcl]]
