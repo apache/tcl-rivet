fbaf3827f0f94d0922a50a7cf3c7d913ae54923f
Bradley A. Morrison <brad@apache.org>
2004-01-15 16:05:48 +0000

    Changed behavior from appending discovered values to existing elements in the response array to simple assignment.

--- rivet/rivet-tcl/load_response.tcl
+++ rivet/rivet-tcl/load_response.tcl
@@ -9,10 +9,6 @@ proc load_response {{arrayName response}} {
     upvar 1 $arrayName response
 
     foreach {var elem} [var all] {
-	if {[info exists response($var)]} {
-	    lappend response($var) $elem
-	} else {
-	    set response($var) $elem
-	}
+	set response($var) $elem
     }
 }
