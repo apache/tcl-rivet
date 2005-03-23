b278b6078b40a1a48dd497fa1443910f3713cfae
David Welton <davidw@apache.org>
2005-03-23 20:50:34 +0000

    * doc/norefchunk.xsl: Removed chunk-first-section-with-parent and   chunk-all-sections
    * genconf.sh: Updated to use 1.8 autotools.

--- genconf.sh
+++ genconf.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 # Be sure to use a recent autotools set.  1.8 works for me.
-aclocal
+
+VERSION="1.8"
+
+if [ "$VERSION" != "" ]
+    then
+    # Add the dash
+    VERSION="-$VERSION"
+fi
+
+aclocal${VERSION}
 autoconf
-automake --add-missing
+automake${VERSION} --add-missing
