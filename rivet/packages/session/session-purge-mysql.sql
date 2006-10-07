7b3dd1df3a6f33a417cd0f0b057a1b3830cdb702
David Welton <davidw@apache.org>
2006-10-07 09:31:18 +0000

    * session-purge-mysql.sql: Updated comments from Massimo.

--- rivet/packages/session/session-purge-mysql.sql
+++ rivet/packages/session/session-purge-mysql.sql
@@ -1,18 +1,22 @@
---  Note
 --
---  9 May 2006
---
---  Though Mysql sql interpreter supports the specification 'ON CASCADE DELETE' for CREATE TABLE, as of
---  version 5.0 this is only for sql code portability: deleting a row in rivet_session doesn't 
---  imply Mysql removes the row in rivet_session_cache sharing the same session_id.
+--  Note  for Mysql 4.x and 5.0 users
+-- 
+--  Though Mysql<5.1 supports the specification 'ON CASCADE DELETE' for CREATE TABLE
+--  this is only for sql code portability: deleting a row in rivet_session doesn't 
+--  imply Mysql remove the rows in rivet_session_cache matching the same session_id.
 --  This table gets progressively cluttered with rows referring to sessions that are long dead.
---  
---  As a possible workaround the programmer may build a cron procedure that runs the following
---  sql statement.
 
-delete rivet_session_cache from rivet_session_cache  left join rivet_session as t2 using(session_id) where t2.session_id is null;
+--  This command can be used to clean up 'rivet_session_cache'
 
---  I don't see any cleaner approach unless the code for the garbage collection in the Session class
---  is modified for sake of portability. (Massimo Manghi)
+delete rivet_session_cache from rivet_session_cache left join rivet_session as t2 using(session_id) where t2.session_id is null;
+
+--  You may subclass 'Session' in order to specialize the method do_garbage_collection and 
+--  exec this command after the superclass method has run.
+
+--  Starting with version 5.1 the 'on delete cascade' should be working (but I haven't tried)
+--  and also triggers can be used to achive the same goal
+
+--  2 Oct 2006 
+
 
 
