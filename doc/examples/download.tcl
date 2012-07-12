195982112c8ce378cebed00495145283eb315258
Massimo Manghi <mxmanghi@apache.org>
2012-07-12 13:37:58 +0000

    * doc/examples/download.tcl: Wrong command 'source' (it had to be 'parse')

--- doc/examples/download.tcl
+++ doc/examples/download.tcl
@@ -14,9 +14,9 @@ if {[var exists pdfname]} {
 # original one. In this case, this is the point where a new file name
 # must be generated.
 
-	headers type			"application/pdf"
-	headers add Content-Disposition "attachment; filename=${pdfname}.pdf"
-	headers add Content-Description "PDF Document"
+        headers type                    "application/pdf"
+        headers add Content-Disposition "attachment; filename=${pdfname}.pdf"
+        headers add Content-Description "PDF Document"
 
 # The pdf is read and stored in a Tcl variable. The file handle is
 # configured for a binary read: we are just shipping raw data to a
@@ -24,22 +24,23 @@ if {[var exists pdfname]} {
 # that is able to retrieve the data to be sent from any data source
 # (e.g. database, external program, other Tcl code)
 
-	set paper	    [open $pdf_full_path r]
-	fconfigure	    $paper -translation binary
-	set pdf		    [read $paper]
-	close $paper
+        set paper       [open $pdf_full_path r]
+        fconfigure      $paper -translation binary
+        set pdf         [read $paper]
+        close $paper
 
 # Now we got the data: let's tell the client how many bytes we are
 # about to send (useful for the download progress bar of a dialog box)
 
-	headers add Content-Length  [string length $pdf]
+        headers add Content-Length  [string length $pdf]
 
 # Let's send the actual file content
 
-	puts $pdf
+        puts $pdf
+
     } else {
-	source pdf_not_found_error.rvt
+        parse pdf_not_found_error.rvt
     }
 } else {
-    source parameter_not_defined_error.rvt
+    parse parameter_not_defined_error.rvt
 }
