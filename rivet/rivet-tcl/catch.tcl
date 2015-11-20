eac668f91e1961cfb8c88ee6c91c0868e28639d1
Massimo Manghi <mxmanghi@apache.org>
2015-11-20 11:30:33 +0000

    2015-11-20 Massimo Manghi <mxmanghi@apache.org>     * rivet/rivet-tcl/catch.tcl: new command [::rivet::catch ...] wraps     the core command but returns an error anyway if raised by     either [::rivet::abort_page] or [::rivet::exit]

---
+++ rivet/rivet-tcl/catch.tcl
@@ -0,0 +1,40 @@
+# catch.tcl
+#
+# Wrapper of the core [::catch] command that checks whether 
+# an error condition is actually raised by [::rivet::abort_page]
+# or [::rivet::exit]. In case the error is thrown again to allow
+# the interpreter to interrupt and pass execution to AbortScript
+#
+# $Id: $
+#
+namespace eval ::rivet {
+
+    proc catch {script args} {
+
+        set catch_cmd [list ::catch $script]
+        if {[llength $args] >= 1} {
+            upvar [lindex $args 0] errvar
+
+            lappend catch_cmd errvar
+
+        }
+        if {[llength $args] >= 2} {
+            upvar [lindex $args 1] catchopt
+
+            lappend catch_cmd catchopt
+        }
+
+        set catch_ret [eval $catch_cmd]
+
+        if {$catch_ret && [::rivet::abort_page -aborting]} {
+
+            return -code error -errorcode ABORTPAGE
+
+        } else {
+
+            return $catch_ret
+
+        }
+
+    }
+}
