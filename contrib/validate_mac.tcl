e179f17fff991e703133062f9979fa1f15cbb634
Massimo Manghi <mxmanghi@apache.org>
2016-10-11 17:01:16 +0000

    * contrib/validate_mac.tcl: example of validator checking     and mangling a network mac address. This function is meant     to work with the FormBroker

---
+++ contrib/validate_mac.tcl
@@ -0,0 +1,37 @@
+# -- validate_mac 
+#
+
+    proc validate_mac {_mac_address_d} {
+        upvar $_mac_address_d mac_address_d
+
+        dict with mac_address_d {
+
+            set var [string trim $var]
+            if {[regexp {^[[:xdigit:]]{2}([:-][[:xdigit:]]{2}){5}$} $var]} {
+
+                set var [string tolower $var]
+
+                # we normalize the mac address to the Unix form.
+                # The dash '-' characters in the windows representation 
+                # are replaced by columns ':'
+
+                set var [regsub -all -- {-} $var :]
+
+                # the 'constrain' field is bidirectional:
+                # it tells the validator to curb/change the value
+                # within bonds/forms/representation. By setting it the
+                # validator tells the Form Broker to copy the value
+                # back in the response array
+
+                set constrain 1
+                return FB_OK
+
+            } else {
+
+                return FB_WRONG_MAC
+
+            }
+
+        }
+    }
+ 
