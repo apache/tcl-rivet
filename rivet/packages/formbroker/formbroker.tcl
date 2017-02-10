dedb94fa70c1798182836e9165d4eb5825e4b2ab
Massimo Manghi <mxmanghi@apache.org>
2017-02-10 22:28:48 +0000

    * rivet/packages/formbroker/formbroker.tcl: now     handling natively also boolean in the form y|n or     0|1

--- rivet/packages/formbroker/formbroker.tcl
+++ rivet/packages/formbroker/formbroker.tcl
@@ -256,6 +256,19 @@ namespace eval FormBroker {
         }
     }
 
+    proc validate_boolean {_var_d} {
+        upvar $_var_d var_d
+
+        dict with var_d {
+            if {[regexp -nocase {Y|N|0|1} $var]} {
+                return FB_OK
+            } else {
+                return FB_INVALID_BOOLEAN
+            }
+        }
+    }
+
+
     proc validate_variable_representation {_var_d} {
         upvar $_var_d var_d
         variable form_definitions
@@ -700,6 +713,9 @@ namespace eval FormBroker {
                     }
                     email {
                         set validator [namespace current]::validate_email
+                    }
+                    boolean {
+                        set validator [namespace current]::validate_boolean
                     }
                     string -
                     default {
