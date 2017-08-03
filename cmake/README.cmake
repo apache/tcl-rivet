e035a54878efde460181aa8a095bf0f509c49f7a
Georgios Petasis <petasis@apache.org>
2017-08-03 18:24:42 +0000

    CMake: More checks for rivet_config.h

--- cmake/README.cmake
+++ cmake/README.cmake
@@ -9,16 +9,16 @@ Compiling Rivet with cmake:
 
 2) Execute the following commands (all commands must be executed from the
    "cmake" directory):
-  
+
    cmake -E make_directory build
-   cmake -E chdir build cmake --config Release ..
-   cmake --build build --target all --config Release --clean-first
+   cmake -E chdir build cmake ..
+   cmake --build build --target all --clean-first
    cmake --build build --target install
 
 3) To install mod_rivet.so/Rivet library to a custom location,
    the following commands can be used:
 
-   cmake -E chdir build cmake --config Release \
+   cmake -E chdir build cmake \
      -DAPACHE_MODULE_DIR=/home/tcl/rivet/branches/cmake/cmake/test/modules \
      -DAPACHE_LIB_DIR=/home/tcl/rivet/branches/cmake/cmake/test/ ..
    cmake --build build --target install
