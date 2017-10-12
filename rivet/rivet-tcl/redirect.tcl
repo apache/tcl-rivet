19f4ae90494da510001ac571ede71bc3ad6fab26
Massimo Manghi <mxmanghi@apache.org>
2017-10-12 22:19:04 +0000

    * doc/xml/processing.xml,command.xml: further elaboration of the     Rivet 3.0 manual. Update ::rivet::redirect manual entry     * tcl/rivet-tcl/redirect.tcl: now handling generic boolean arguments     or any integer > 0 (as for the HTTP status codes)

--- rivet/rivet-tcl/redirect.tcl
+++ rivet/rivet-tcl/redirect.tcl
@@ -8,7 +8,11 @@
 #
 #   - url               - URL to which we are redirecting the client
 #   - permanent:[0 | 1] - whether redirection will be permanent (default)
+#     or
+#   - permanent: code   - returns any HTTP integer code. In this context
+#                         only the 3xx status codes are meaningful
 #
+#   $ Id: $
 #
 
 namespace eval ::rivet {
@@ -20,11 +24,37 @@ namespace eval ::rivet {
             return  -code error \
                     -errorcode headers_already_sent \
                     -errorinfo "Impossible to redirect: headers already sent"
+
+        }
+
+        # In order to preserve compatibility 
+        # with the past we chec whether we are
+        # dealing with a boolean argument and handle
+        # it accordingly 
+
+        if {[string is boolean $permanent] } {
+
+            if {[string is true $permanent]} {
+                set http_code 301
+            } else {
+                set http_code 302
+            } 
+
+        } elseif {[string is integer $permanent] && ($permanent > 0)} {
+
+            set http_code $permanent
+
+        } else {
+
+            return  -code error \
+                    -errorcode invalid_http_code \
+                    -errorinfo "Invalid HTTP status code: $permanent"
+
         }
 
         ::rivet::no_body ; ## don’t output anything on a redirect
         ::rivet::headers set Location $url
-        ::rivet::headers numeric [expr {$permanent ? "301" : "302"}]
+        ::rivet::headers numeric $http_code
         ::rivet::abort_page [dict create error_code redirect location $url] ; ## stop any further processing
 
         return -error ok 
