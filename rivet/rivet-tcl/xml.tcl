fc8aa80adaf78dfef128e3df76c6d6a51b9deb32
Massimo Manghi <mxmanghi@apache.org>
2018-08-16 18:15:48 +0200

    now printing XML elements such as  <tag attr1=val1 attr2=val2.... /> if the text string is empty

--- rivet/rivet-tcl/xml.tcl
+++ rivet/rivet-tcl/xml.tcl
@@ -4,19 +4,22 @@
 # Example 1:
 #
 #  ::rivet::xml Test b i 
-# <== <b><i>Test</i></b>
+# <= <b><i>Test</i></b>
 #  
 # Example 2:
 #
 # ::rivet::xml Test [list div class box id testbox] b i
-# <== <div class="box" id="testbox"><b><i>Test</i></b></div>
+# <= <div class="box" id="testbox"><b><i>Test</i></b></div>
 #
 # Example 3
 #
 # ::rivet::xml "anything ..." div [list a href "http://..../" title "info message"] 
-# <== <div><a href="http://..../" title="info message">anything ...</a></div>
+# <= <div><a href="http://..../" title="info message">anything ...</a></div>
 #
-# $Id$
+# Example 4
+#
+# ::rivet::xml "" [list a attr1 val1 attr2 val2] [list b attr1 val1 attr2 val2]
+# <= <a attr1="val1" attr2="val2"><b attr1="val1" attr2="val2"/></a>
 #
 
 namespace eval ::rivet {
@@ -39,10 +42,19 @@ namespace eval ::rivet {
             append xmlout ">"
         }
 
+
         if {[::rivet::lempty $tags_stack]} {
+
             return $textstring
+
         } else {
+
+            if {$textstring == ""} {
+                set xmlout [string replace $xmlout end end " />"]
+                return [append xmlout "</[join [lreverse [lrange $tags_stack 0 end-1]] "></"]>"]
+            }
             return [append xmlout "$textstring</[join [lreverse $tags_stack] "></"]>"]
+
         }
     }
 
