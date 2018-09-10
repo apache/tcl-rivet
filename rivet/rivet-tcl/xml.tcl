b604e3d8654e5d8546a285f569584c748e8de0ff
Massimo Manghi <mxmanghi@apache.org>
2018-09-10 18:02:33 +0200

    changing the ::rivet::xml form to enable selfclosing elements generation

--- rivet/rivet-tcl/xml.tcl
+++ rivet/rivet-tcl/xml.tcl
@@ -1,12 +1,24 @@
 #
-# xml.tcl string ?tag ?attr val? ?attr val?? ?tag ?attr val? ?attr val??
+# xml.tcl --
+#
+# Syntax:
+#
+#       ::rivet::xml string ?tag ?attr val? ?attr val?? ?tag ?attr val? ?attr val??
+#
+# or
+#
+#       ::rivet::xml ?tag ?attr val? ?attr val??
 #
 # Example 1:
+#
+# trivial nested markup fragment
 #
 #  ::rivet::xml Test b i 
 # <= <b><i>Test</i></b>
 #  
 # Example 2:
+#
+# XHTML Element with attributes
 #
 # ::rivet::xml Test [list div class box id testbox] b i
 # <= <div class="box" id="testbox"><b><i>Test</i></b></div>
@@ -19,28 +31,47 @@
 # Example 4
 #
 # ::rivet::xml "" [list a attr1 val1 attr2 val2] [list b attr1 val1 attr2 val2]
-# <= <a attr1="val1" attr2="val2"><b attr1="val1" attr2="val2"/></a>
+# <= <a attr1="val1" attr2="val2"><b attr1="val1" attr2="val2"></b></a>
+#
+# Example 5
+#
+# single self closing element
+#
+# ::rivet::xml [list a attr1 val1 attr2 val2]
+# <= <a attr1="val1" attr2="val2" />
 #
 
 namespace eval ::rivet {
 
     proc xml {textstring args} {
 
-        set xmlout      ""
-        set tags_stack  {}
+        set single_element [::rivet::lempty $args]
+        if {$single_element} {
 
-        foreach el $args {
+            set tags_list   [list $textstring]
+
+            if {[::rivet::lempty $tags_list]} { return "" }
+
+        } else {
+
+            set tags_list $args
+
+        }
+
+        set tags_stack  {}
+        set el          {}
+        set xmlout      ""
+        foreach el $tags_list {
 
             set el  [lassign $el tag]
             lappend tags_stack $tag
-            append xmlout "<$tag"
+            append  xmlout "<$tag"
 
             foreach {attrib attrib_v} $el {
                 append xmlout " $attrib=\"$attrib_v\""
             }
 
             append xmlout ">"
-            set last_elem_attr $el
         }
 
 
@@ -48,24 +79,26 @@ namespace eval ::rivet {
 
             return $textstring
 
+        } elseif {$single_element} {
+
+            # variable 'el' keeps the last (innermost) attribute-value list
+
+            if {[::rivet::lempty $el] == 1} {
+                set xmlout [string replace $xmlout end end " />"]
+            } else {
+                set xmlout [string replace $xmlout end end "/>"]
+            }
+
+            if {[llength $tags_stack] > 1} {
+                set xmlout [append xmlout "</[join [lreverse [lrange $tags_stack 0 end-1]] "></"]>"]
+            }
+            return $xmlout
+
         } else {
 
-            if {$textstring == ""} {
-
-                if {[llength $last_elem_attr]} { 
-                    set closing_seq "/>"
-                } else {
-                    set closing_seq " />"
-                }
-                set xmlout [string replace $xmlout end end $closing_seq]
-                if {[llength $tags_stack] > 1} {
-                    set xmlout [append xmlout "</[join [lreverse [lrange $tags_stack 0 end-1]] "></"]>"]
-                }
-                return $xmlout
-
-            }
             return [append xmlout "$textstring</[join [lreverse $tags_stack] "></"]>"]
 
         }
     }
+
 }
--- tests/commands.tcl
+++ tests/commands.tcl
@@ -10,9 +10,18 @@ if {[::rivet::var exists cmd]} {
     set cmd [::rivet::var get cmd]
     switch $cmd {
         xml {
-            puts [::rivet::xml "" a [list b a1 v1 a2 v2] [list c a1 v1 a2 v2]]
+
+            # generic ::rivet::xml usage
+
+            puts [::rivet::xml "a text string" a [list b a1 v1 a2 v2] [list c a1 v1 a2 v2]]
+            puts [::rivet::xml "a text string" [list b a1 v1 a2 v2] [list c a1 v1 a2 v2] a]
+
             puts [::rivet::xml "" [list b a1 v1 a2 v2] [list c a1 v1 a2 v2] a]
-            puts -nonewline [::rivet::xml "element text" a [list b a1 v1 a2 v2] [list c a1 v1 a2 v2]]
+
+            # self closing single element
+
+            puts [::rivet::xml [list a a1 v1 a2 v2]]
+            puts -nonewline [::rivet::xml p]
         }
         default {
             puts "invalid argument '$cmd'"
