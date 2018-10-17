6f1027745ca8f3bdc7b2cec8000af3a42f9226de
Massimo Manghi <mxmanghi@apache.org>
2018-10-17 11:31:55 +0200

    * tests/commands.tcl: changing test on self closing element * [doc/rivet.xml.in|doc/rivet.xml]: removed delimiters of symbols expanded by svn * doc/xml/commands.xml: fixed entities in ::rivet::xml manual page

--- tests/commands.tcl
+++ tests/commands.tcl
@@ -15,13 +15,12 @@ if {[::rivet::var exists cmd]} {
 
             puts [::rivet::xml "a text string" a [list b a1 v1 a2 v2] [list c a1 v1 a2 v2]]
             puts [::rivet::xml "a text string" [list b a1 v1 a2 v2] [list c a1 v1 a2 v2] a]
-
             puts [::rivet::xml "" [list b a1 v1 a2 v2] [list c a1 v1 a2 v2] a]
 
             # self closing single element
 
             puts [::rivet::xml [list a a1 v1 a2 v2]]
-            puts -nonewline [::rivet::xml p]
+            puts -nonewline [::rivet::xml br]
         }
         default {
             puts "invalid argument '$cmd'"
