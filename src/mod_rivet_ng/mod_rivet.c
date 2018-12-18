617e24b5e34ede7d2874eee5333893ab77190ef1
Massimo Manghi <mxmanghi@apache.org>
2018-12-18 10:29:32 +0100

    commit pkgIndex.tcl update

--- src/mod_rivet_ng/mod_rivet.c
+++ src/mod_rivet_ng/mod_rivet.c
@@ -226,7 +226,7 @@ static int
 Rivet_RunServerInit (apr_pool_t *pPool, apr_pool_t *pLog, apr_pool_t *pTemp, server_rec *s)
 {
 #ifdef WIN32
-	char*			   parent_pid_var = NULL;
+	char* parent_pid_var = NULL;
 #endif
     rivet_server_conf* rsc = RIVET_SERVER_CONF( s->module_config );
 
