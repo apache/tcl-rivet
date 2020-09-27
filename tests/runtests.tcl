a522d75d070fcaaaf9fea6dd56397dd9148629ac
Massimo Manghi <mxmanghi@apache.org>
2020-09-27 23:52:14 +0200

    add support for testing with different MPMs and bridges

--- tests/runtests.tcl
+++ tests/runtests.tcl
@@ -16,8 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# $Id$
-
 set auto_path [linsert $auto_path 0 [file join [file dirname [info script]] apachetest]]
 set default_mpm prefork
 set httpd_args   {}
