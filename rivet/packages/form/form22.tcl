b33ece8fc7a18625eee1e544ab2f4677d9a58fe4
Massimo Manghi <massimo.manghi@gmail.com>
2021-11-06 17:48:26 +0100

    documenting new switch -emit of package form 2.2

--- rivet/packages/form/form22.tcl
+++ rivet/packages/form/form22.tcl
@@ -1,6 +1,6 @@
 # form.tcl -- generate forms automatically.
 
-# Copyright 2002-2004 The Apache Software Foundation
+# Copyright 2002-2021 The Apache Software Foundation
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
